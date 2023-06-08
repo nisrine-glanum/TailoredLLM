f5df6a2d726359006c56c8d729a6bdaac072716b
Nisrine <nisrine@glanum.com>
2023-06-08 10:45:38 +0200

    LightOn LLM

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ Created on Tue Apr 25 12:43:13 2023
 import streamlit as st
 import requests
 import os
-import dotenv
+from paradigm_client.remote_model import RemoteModel
+from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -24,7 +25,7 @@ st.divider()
 
 add_selectbox = st.sidebar.selectbox(
     "NLP tasks :",
-    ("Summarization", "Backlog")
+    ("Summarization", "LightOn LLM", "Backlog")
 )
 
 other_selectbox = st.sidebar.selectbox(
@@ -52,6 +53,45 @@ if add_selectbox == "Backlog":
     st.download_button('Download', text_contents)
     
     st.divider()
+
+host = os.environ.get('HOST')
+model = RemoteModel(host, model_name="llm-mini")
+
+lighton_model = {
+    '' : '',
+    'LightOn' : model,
+}
+
+if add_selectbox == "LightOn LLM":
+    st.warning("LightOn LLM")
+    txt = st.text_area('Put the text to FALC here')
+
+    option = st.selectbox(
+        'Choose a model',
+        (list(lighton_model.keys()))
+    )
+
+    if st.button('Click here to FALC the text'):
+        if option == "Open-assistant":
+            output = query(
+                {"inputs": "<|prompter|> " + txt + ". Peux-tu faire un résumé d'un texte ?<|endoftext|><|assistant|>"},
+                str(lighton_model[option]))
+            try:
+                st.write("The simplified version of your text is :")
+                st.success(output[0]["summary_text"])
+                text_contents = output[0]["summary_text"]
+                st.download_button('Download', text_contents)
+            except KeyError:
+                st.error(output["error"] + ". \n\n Wait a moment for the model to load and retry later !", icon="🚨")
+        elif (option != '') and (option in lighton_model):
+            output = query({"inputs": txt}, str(lighton_model[option]))
+            try:
+                st.write("The simplified version of your text is :")
+                st.success(output[0]["summary_text"])
+                text_contents = output[0]["summary_text"]
+                st.download_button('Download', text_contents)
+            except KeyError:
+                st.error(output["error"] + ". \n\n Wait a moment for the model to load and retry later !", icon="🚨")
 
 summarization_models = {
     '' : '',
