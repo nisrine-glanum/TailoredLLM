9e204ad1f6faa169a25fc4c24e94f161306e1953
Nisrine <nisrine@glanum.com>
2023-06-08 11:38:52 +0200

    LightOn LLM

--- app.py
+++ app.py
@@ -54,6 +54,8 @@ if add_selectbox == "Backlog":
     
     st.divider()
 
+os.environ["PARADIGM_API_KEY"] = "yqa3tBQp.YNXQ5aWtr5j1ZX97cGLV9gFgbphNFOnE"
+os.environ["HOST"] = "https://llm.lighton.ai"
 host = os.environ.get('HOST')
 model = RemoteModel(host, model_name="llm-mini")
 
